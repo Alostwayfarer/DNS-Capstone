af65efbeb7fb6bbb68472e1f41342dc17b155d9e
dikshant sharma <dikshantks229@gmail.com>
2024-11-16 16:08:12 +0530

    changed foler name

--- build-server api/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
-const { exec } = require("child_process");
-const AWS = require("aws-sdk");
-
-const app = express();
-app.use(express.json());
