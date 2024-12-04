c0c757e961209d82d68e171e6378498023c60eee
dikshant sharma <dikshantks229@gmail.com>
2024-12-04 12:11:42 +0530

    capapblilties

---
+++ frontend/capabilites.ts
@@ -0,0 +1,14 @@
+exports.capabilities = {
+    "browserName": "Chrome",
+    "browserVersion": "dev",
+    "LT:Options": {
+        "username": "dikshantks229",
+        "accessKey": "4TaO9BhutZPtuKuHHApfD7AcXa8fqKGAnBTM4MTBSC31Esmc5P",
+        "platformName": "Windows 10",
+        "build": "dns-build",
+        "project": "dns",
+        "name": "one",
+        "w3c": true,
+        "plugin": "node_js-mocha"
+    }
+};
